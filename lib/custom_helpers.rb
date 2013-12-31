cb74ee7ff18d30c58d3680731c197a4fb6fff96b
Alex Tacho <hemmshoe@gmail.com>
2013-12-31 12:33:46 +0100

    added bootstrap and created 2 column layout

--- lib/custom_helpers.rb
+++ lib/custom_helpers.rb
@@ -3,6 +3,10 @@ module CustomHelpers
     sitemap.resources.find_all{|p| p.source_file.match(/articles\/.*\.html/)}.group_by{|p| replace_with_whitespace p.path.split('/')[1]}
   end
 
+  def articles_for group
+    sitemap.resources.find_all{|p| p.source_file.match(/articles\/.*\.html/)}.group_by{|p| replace_with_whitespace p.path.split('/')[1]}
+  end
+
   def article_title article
     article.data.title || replace_with_whitespace(article.path.split('/').last.gsub '.html', '' )
   end
