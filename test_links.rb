4fb727b842be54ea245403b40c968b7fd7ed88a3
Alex Tacho <hemmshoe@gmail.com>
2014-01-26 20:00:28 +0100

    updated formating of test script

--- test_links.rb
+++ test_links.rb
@@ -9,9 +9,8 @@ Dir.glob("build/documentation/**/*.html") do |html_file|
       next if parts.nil?
       parts.each do |part|
         if !part.include? "documentation"
-          puts "\e[0;31mlink error\e[m\n"
+          puts "\e[0;31mlink error:\e[m"
           puts "\e[0;33m#{html_file}:#{line_counter}\e[m => " + part[1..part.length-2]
-          puts "\n\r"
           errors_count = errors_count + 1
         end
       end
