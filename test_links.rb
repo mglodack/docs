2832a56df064879016cb0477594d733e34e95ef8
Alex Tacho <hemmshoe@gmail.com>
2014-01-26 19:51:11 +0100

    updated test file

--- test_links.rb
+++ test_links.rb
@@ -1,5 +1,6 @@
+errors_count = 0
+
 Dir.glob("build/documentation/**/*.html") do |html_file|
-  errors_count = 0
   File.open(html_file, "r") do |infile|
     line_counter = 0
     while (line = infile.gets)
@@ -8,15 +9,16 @@ Dir.glob("build/documentation/**/*.html") do |html_file|
       next if parts.nil?
       parts.each do |part|
         if !part.include? "documentation"
-          puts "link error in file #{html_file}:#{line_counter} => " + part
+          puts "\e[0;31mlink error in file \e[m"
+          puts "\e[0;33m#{html_file}:#{line_counter}\e[m => " + part[1..part.length-2]
           errors_count = errors_count + 1
         end
       end
     end
   end
+end
 
-  if errors_count > 0
-    puts "#{errors_count} link errors in documentation"
-    exit 1
-  end
+if errors_count > 0
+  puts "\e[0;31m#{errors_count} link errors in documentation\e[m"
+  exit 1
 end
