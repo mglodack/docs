e6fd7f78543b40f6cf26e26925a546e9c9430a25
Alex Tacho <hemmshoe@gmail.com>
2014-01-28 12:46:58 +0100

    fixed toggling of off canvas on mobile

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,3 +1,7 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+// workaround: there needs to be a click event to make the toggling of off canvas work on mobile devices
+// http://foundation.zurb.com/forum/posts/1651-cant-get-off-canvas-to-toggle-on-mobile
+$('a.left-off-canvas-toggle').on('click',function(){});
